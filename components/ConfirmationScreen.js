bd5e46935e023611d41dce32fed1f02d9d676139
Maxim Zhukov <zhkv.mxm@gmail.com>
2016-04-23 11:33:09 +0600

    Update ConfirmationScreen content and styles

--- components/ConfirmationScreen.js
+++ components/ConfirmationScreen.js
@@ -5,6 +5,7 @@ import React, {
   Modal,
   StyleSheet,
   Text,
+  TouchableNativeFeedback,
   View
 } from 'react-native';
 
@@ -12,8 +13,32 @@ class ConfirmationScreen extends Component {
   render() {
     return (
       <Modal>
-        <View style={styles.window}>
-          <Text>Please confirm...</Text>
+        <View style={styles.container}>
+          <View style={styles.window}>
+            <Text style={styles.header}>
+              Delete record?
+            </Text>
+            <View style={styles.buttons}>
+              <TouchableNativeFeedback
+                background={TouchableNativeFeedback.SelectableBackground()}
+              >
+                <View style={styles.button}>
+                  <Text style={styles.button__text}>
+                    Cancel
+                  </Text>
+                </View>
+              </TouchableNativeFeedback>
+              <TouchableNativeFeedback
+                background={TouchableNativeFeedback.SelectableBackground()}
+              >
+                <View style={styles.button}>
+                  <Text style={styles.button__text}>
+                    Delete
+                  </Text>
+                </View>
+              </TouchableNativeFeedback>
+            </View>
+          </View>
         </View>
       </Modal>
     );
@@ -21,11 +46,31 @@ class ConfirmationScreen extends Component {
 }
 
 const styles = StyleSheet.create({
-  window: {
+  container: {
     flex: 1,
-    margin: 16,
+    justifyContent: 'center',
+  },
+  window: {
+    margin: 32,
     padding: 16,
     backgroundColor: '#fff',
+  },
+  header: {
+    fontSize: 22,
+    marginBottom: 22,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  },
+  button: {
+    paddingTop: 8,
+    paddingRight: 16,
+    paddingBottom: 8,
+    paddingLeft: 16,
+  },
+  button__text: {
+    fontSize: 16
   },
 });
 
